e2e87f6c7821af731adb669616c1c51b9f41b30b
zhengjun <330172829@qq.com>
2017-10-18 22:12:43 +0800

    Initial Commit

---
+++ Bush01/Bush01.h
@@ -0,0 +1,19 @@
+//
+//  Bush01.h
+//  Bush01
+//
+//  Created by Caspar on 10/18/17.
+//  Copyright © 2017 Caspar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Bush01.
+FOUNDATION_EXPORT double Bush01VersionNumber;
+
+//! Project version string for Bush01.
+FOUNDATION_EXPORT const unsigned char Bush01VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bush01/PublicHeader.h>
+
+
